ea411d8597aefb9f5f1dd728f0f36ea1dee491c6
GH0STAV0 <you@example.com>
2021-11-29 19:26:03 -0800

    add new project

--- go.py
+++ go.py
@@ -1,6 +1,5 @@
 import os
 import mod_vpn2
-
-
+mod_vpn2.fnc_vpn ()
 
 os.system("sh -c './honeygain -tou-accept -email y0shimitsugh0st84@gmail.com -pass testpassw0rd -device DEVICE_PNV_SYD' 2>&1 | tee file.txt")
